1fecdad27d3ee86dd1fb2737a1b61199bb6a36ba
KostasMparmparousis <mparmparousis.kostas@gmail.com>
2022-04-04 12:10:04 +0300

    Drawing architecture

--- src/main/java/gr/gunet/loginNameValidityChecker/ServerConfigurations.java
+++ src/main/java/gr/gunet/loginNameValidityChecker/ServerConfigurations.java
@@ -8,9 +8,9 @@ public class ServerConfigurations {
     public static String getConfiguration(String confParam){
         if(configurations == null){
             configurations = new HashMap();
-//            configurations.put("base_url", "https://ulookup.gunet.gr");
+            configurations.put("base_url", "https://ulookup.gunet.gr");
 //            configurations.put("base_url", "https://logassign.gunet.gr");
-            configurations.put("base_url", "http://localhost:4567");
+//            configurations.put("base_url", "http://localhost:4567");
         }
         return configurations.get(confParam);
     }
